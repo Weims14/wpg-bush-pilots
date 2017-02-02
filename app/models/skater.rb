4beaa190c5d8a7ba71d8ee9f69211c535db6d943
Brendan Weimer <brendanweimer@gmail.com>
2017-02-02 15:37:37 -0600

    Created a way for points to automatically be calculated when goals and assists are calculated

--- app/models/skater.rb
+++ app/models/skater.rb
@@ -1,3 +1,13 @@
 class Skater < ApplicationRecord
-  
+  # def initialize
+  #   goals   ||= 0
+  #   assists ||= 0
+  # end
+  def points
+    if goals.blank? || assists.blank?
+      0
+    else
+      goals + assists
+    end
+  end
 end
