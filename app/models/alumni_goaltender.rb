2c6dd568646b0047d7728fffb10e651d4d97501e
Brendan Weimer <brendanweimer@gmail.com>
2017-04-06 09:03:54 -0500

    Modified the alumni goalie and skater models to auto add stats

--- app/models/alumni_goaltender.rb
+++ app/models/alumni_goaltender.rb
@@ -1,2 +1,27 @@
 class AlumniGoaltender < ApplicationRecord
+  def gp
+    if wins.blank? || loses.blank?
+      0
+    else
+      wins + loses
+    end
+  end
+
+  def winpercent
+    if wins.blank? || gp.blank?
+      0
+    else
+    winpercent = wins.to_f / gp.to_f
+    winpercent.round(3)
+    end
+  end
+
+  def gaa
+    if gp.blank? || ga.blank?
+      0
+    else
+    gaa = ga.to_f / gp.to_f
+    gaa.round(2)
+    end
+  end
 end
--- app/models/alumni_skater.rb
+++ app/models/alumni_skater.rb
@@ -1,2 +1,9 @@
 class AlumniSkater < ApplicationRecord
+  def points
+    if goals.blank? || assists.blank?
+      0
+    else
+      goals + assists
+    end
+  end
 end
